28f226f080a7a04f690c2d51a7bd48d4833befad
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-05-07 14:58:41 -0300

    criação da main 	modified:   Hotel_Simulation/src/Main/Main.java

--- Hotel_Simulation/src/Main/Main.java
+++ Hotel_Simulation/src/Main/Main.java
@@ -1,4 +1,25 @@
 package Main;
 
+import HotelEntities.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        final int NUM_GUESTS = 50;
+        List<Guest> guests = new ArrayList<>();
+        Hotel hotel = new Hotel();
+
+        // Criando hóspedes
+        for (int i = 0; i < NUM_GUESTS; i++) {
+            Guest guest = new Guest(i + 1, hotel);
+            guests.add(guest);
+        }
+
+        // Iniciando as threads dos hóspedes
+        for (Guest guest : guests) {
+            guest.start();
+        }
+    }
 }
