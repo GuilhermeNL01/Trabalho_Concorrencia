8de97435ea03d8bd55f75f29070a7043a36330c9
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-05-07 12:01:13 -0300

    Adição dos métodos e atributos da classe Key

--- Hotel_Simulation/src/HotelEntities/Key.java
+++ Hotel_Simulation/src/HotelEntities/Key.java
@@ -1,4 +1,22 @@
 package HotelEntities;
 
 public class Key {
+    private Room room; // Referência para o quarto associado a esta chave
+    private int keyId; // Identificador único da chave
+
+    // Construtor
+    public Key(Room room){
+        this.room = room; // Define o quarto associado a esta chave
+        this.keyId = room.getNumber(); // O identificador da chave é o número do quarto
+    }
+
+    // Método para obter o identificador da chave
+    public int getKeyId() {
+        return keyId;
+    }
+
+    // Método para obter o quarto associado a esta chave
+    public Room getRoom() {
+        return room;
+    }
 }
