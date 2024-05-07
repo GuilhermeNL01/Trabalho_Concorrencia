0f7f81efcd6d8a7719215d167518066ff852a8bd
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-05-07 11:53:04 -0300

    criando e construindo as camareiras/housekeepers 	modified:   Hotel_Simulation/src/HotelEntities/Housekeeper.java

--- Hotel_Simulation/src/HotelEntities/Housekeeper.java
+++ Hotel_Simulation/src/HotelEntities/Housekeeper.java
@@ -1,4 +1,19 @@
-package HotelEntities;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Housekeeper {
+public class Housekeeper extends Thread{
+    private final int id;
+    private boolean isCleaning;
+    private final Hotel hotel;
+    private boolean isAvailable = true;
+    private Room beingCleaned;
+    private Lock lock;
+
+    public Housekeeper(int id, Hotel hotel) {
+        this.id = id;
+        this.hotel = hotel;
+        this.isCleaning = false;
+        this.lock = new ReentrantLock();
+    }
 }
