8de928820daf790fdbf683136169e91b173bd987
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-05-07 14:42:30 -0300

    inicialização da classe room 	modified:   Hotel_Simulation/src/HotelEntities/Room.java

--- Hotel_Simulation/src/HotelEntities/Room.java
+++ Hotel_Simulation/src/HotelEntities/Room.java
@@ -1,4 +1,28 @@
 package HotelEntities;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Room {
+    private final int number;
+    private final int capacity;
+    private final Lock lock;
+    private boolean available;
+    private boolean hasKey;
+    private boolean beingCleaned;
+    public boolean isClean;
+    public Guest guest = null;
+    public Key key;
+
+    // Commit: "Inicialização da Classe Room"
+    // Comentário: "Adiciona a implementação básica da classe Room com os atributos e métodos necessários."
+    public Room(int number, int capacity) {
+        this.number = number;
+        this.capacity = capacity;
+        this.lock = new ReentrantLock();
+        this.available = true;
+        this.isClean = true;
+        this.beingCleaned = false;
+        key = new Key(this);
+    }
 }
