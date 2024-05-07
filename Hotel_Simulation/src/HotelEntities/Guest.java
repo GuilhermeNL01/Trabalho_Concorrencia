09e234aba8f5d278ef44fee2aa0e78059c1c4b74
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-05-07 11:24:18 -0300

    Definir quarto chave e reclamações 	modified:   Hotel_Simulation/src/HotelEntities/Guest.java

--- Hotel_Simulation/src/HotelEntities/Guest.java
+++ Hotel_Simulation/src/HotelEntities/Guest.java
@@ -19,3 +19,20 @@ public class Guest extends Thread {
         this.room = null;
         this.thread = new Thread(this);
         this.FamilyMembers = new Random().nextInt(4); // Define o número aleatório de membros da família
+    }
+
+    // Define o quarto para o hóspede
+    public void setRoom(Room room){
+        this.room = room;
+    }
+
+    // Define a chave para o hóspede
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    // Obtém o número de tentativas de reclamação
+    public int getComplaintAttempts() {
+        return complaintAttempts++;
+    }
+}
