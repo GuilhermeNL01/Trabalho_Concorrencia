99c1b31b8bfc4f83d4bebce729b611970683e85d
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-05-07 11:22:19 -0300

    Criação e construção da classe Guest 	modified:   Hotel_Simulation/src/HotelEntities/Guest.java

--- Hotel_Simulation/src/HotelEntities/Guest.java
+++ Hotel_Simulation/src/HotelEntities/Guest.java
@@ -1,4 +1,21 @@
 package HotelEntities;
 
-public class Guest {
-}
+import java.util.Random;
+
+public class Guest extends Thread {
+    private final int id;
+    private final Hotel hotel;
+    private Room room;
+    private boolean isHosted = false;
+    private Thread thread;
+    private Key key;
+    private int complaintAttempts = 2;
+    private int FamilyMembers; // Variável de membro da família
+
+    // Construtor
+    public Guest(int id, Hotel hotel) {
+        this.id = id;
+        this.hotel = hotel;
+        this.room = null;
+        this.thread = new Thread(this);
+        this.FamilyMembers = new Random().nextInt(4); // Define o número aleatório de membros da família
